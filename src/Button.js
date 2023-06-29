f8af26403f3b4e2a662deacf301341132206850e
Jarod Kober <jarod.development@gmail.com>
2023-06-29 13:47:45 -0700

    Defined Props & Validation for Button Component
    * Defined button types
    * Used prop-types to create validation of props passed to Button component

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,26 @@
-function Button() {
-	return <button>Hi there!</button>;
+import PropTypes from 'prop-types';
+
+function Button({ children, primary, secondary, success, warning, danger, outline, rounded }) {
+	return <button>{children}</button>;
 }
+
+Button.propTypes = {
+	primary: PropTypes.bool,
+	secondary: PropTypes.bool,
+	success: PropTypes.bool,
+	warning: PropTypes.bool,
+	danger: PropTypes.bool,
+	outline: PropTypes.bool,
+	rounded: PropTypes.bool,
+
+	checkVariationValue: (primary, secondary, success, warning, danger) => {
+		const count =
+			Number(!!primary) + Number(!!secondary) + Number(!!success) + Number(!!warning) + Number(!!danger);
+
+		if (count > 1) {
+			return new Error('Invalid props passed to Button component: There can only be a single variant.');
+		}
+	}
+};
 
 export default Button;
