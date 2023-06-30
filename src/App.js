be3b80e62318a430b7ed8e5d8d884d06f3cd61f5
Jarod Kober <jarod.development@gmail.com>
2023-06-29 17:01:38 -0700

    Created Button Styles
    * Used the classnames utility to concatenate CSS classes for button styles
    * Styles are conditional based on the value of props passed into the Button component
    * Created sample buttons in the App component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,40 @@ function App() {
 	return (
 		<div>
 			<div>
-				<Button></Button>
+				<Button
+					success
+					outline
+					rounded
+				>
+					Click me!!
+				</Button>
+			</div>
+			<div>
+				<Button
+					danger
+					outline
+				>
+					Buy Now!
+				</Button>
+			</div>
+			<div>
+				<Button warning>See Deal!</Button>
+			</div>
+			<div>
+				<Button
+					secondary
+					outline
+				>
+					Hide Ads!
+				</Button>
+			</div>
+			<div>
+				<Button
+					primary
+					rounded
+				>
+					Something!
+				</Button>
 			</div>
 		</div>
 	);
