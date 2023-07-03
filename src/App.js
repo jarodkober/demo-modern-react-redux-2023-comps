c5566396c3085764c9fe51f458fea440b63a411d
Jarod Kober <jarod.development@gmail.com>
2023-07-03 13:38:47 -0700

    Created Accordion Component
    * Created the Accordion component, which accepts an array of objects as props
    * Mapped over props to build an accordion element with unique ids, label, and content
    * Created state to manage which accordion element is expanded
    * Updated the App component with props to pass into the Accordion content
    * The Button content removed from the App component was moved to the ButtonPage component in a previous commit

--- src/App.js
+++ src/App.js
@@ -1,53 +1,28 @@
-import { GoBell, GoCodespaces, GoDatabase } from 'react-icons/go';
-import Button from './components/Button';
+import Accordion from './components/Accordion';
 
 function App() {
-	return (
-		<div>
-			<div>
-				<Button
-					secondary
-					outline
-					rounded
-					className="mb-5"
-				>
-					<GoBell />
-					Click me!!
-				</Button>
-			</div>
-			<div>
-				<Button
-					danger
-					outline
-				>
-					<GoCodespaces />
-					Buy Now!
-				</Button>
-			</div>
-			<div>
-				<Button warning>
-					<GoDatabase />
-					See Deal!
-				</Button>
-			</div>
-			<div>
-				<Button
-					secondary
-					outline
-				>
-					Hide Ads!
-				</Button>
-			</div>
-			<div>
-				<Button
-					primary
-					rounded
-				>
-					Something!
-				</Button>
-			</div>
-		</div>
-	);
+	const items = [
+		{
+			id: 'l2kj5',
+			label: 'Can I use React on a project?',
+			content:
+				'You can use React on any project you want. You can use React on any project you want.'
+		},
+		{
+			id: 'l2kj6',
+			label: 'Can I use JavaScript on a project?',
+			content:
+				'You can use React on any project you want. You can use React on any project you want.'
+		},
+		{
+			id: 'l2kj7',
+			label: 'Can I use CSS on a project?',
+			content:
+				'You can use React on any project you want. You can use React on any project you want.'
+		}
+	];
+
+	return <Accordion items={items} />;
 }
 
 export default App;
