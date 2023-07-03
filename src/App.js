93662bce8135c3fbb4d746d29b14d1cea9acb91e
Jarod Kober <jarod.development@gmail.com>
2023-07-03 16:41:50 -0700

    Created the Dropdown Component
    * Created the Dropdown component, which accepts onChange, options, and value as props from the parent
    * Created isOpen state to manage whether or not the dropdown is open
    * Created the handleOptionClick event handler to toggle the dropdown open/closed and return the selected value to the callback function
    * Mapped over the options prop to build the dropdown with values and labels
    * Applied Tailwind CSS classes for styling
    * Imported chevron from React Icons
    * Updated the App component to define the callback function, state of selected value, and array of dropdown options

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
-function App() {}
+import { useState } from 'react';
+import Dropdown from './components/Dropdown';
+
+function App() {
+	const [selection, setSelection] = useState(null);
+
+	const handleSelect = (option) => {
+		setSelection(option);
+	};
+
+	const options = [
+		{ label: 'Red', value: 'red' },
+		{ label: 'Green', value: 'green' },
+		{ label: 'Blue', value: 'blue' }
+	];
+
+	return (
+		<Dropdown
+			onChange={handleSelect}
+			options={options}
+			value={selection}
+		/>
+	);
+}
 
 export default App;
