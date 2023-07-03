20ff2766f0aa9258b288666857a1f6cc2aef9c89
Jarod Kober <jarod.development@gmail.com>
2023-07-03 14:48:48 -0700

    Switched Accordion to Functional State Update
    * Updated the click handler to use a functional state update, since the new value depends on the old
    * This was unlikely to be a real world bug since a user can't click faster than React delayed state updates, but better safe than sorry

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -5,11 +5,13 @@ function Accordion({ items }) {
 	const [expandedIndex, setExpandedIndex] = useState(-1);
 
 	const handleClick = (index) => {
-		if (expandedIndex === index) {
-			setExpandedIndex(-1);
-		} else {
-			setExpandedIndex(index);
-		}
+		setExpandedIndex((currentExpandedIndex) => {
+			if (currentExpandedIndex === index) {
+				return -1;
+			} else {
+				return index;
+			}
+		});
 	};
 
 	const renderedItems = items.map((item, index) => {
