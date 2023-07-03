996aa7fb8e5a4f356a763f92aa8862e3ae0d0d6c
Jarod Kober <jarod.development@gmail.com>
2023-07-03 15:20:08 -0700

    Shifted Accordion to New Accordion Page Component
    * Created new AccordionPage component
    * Moved accordion logic in App component to AccordionPage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-import Accordion from './components/Accordion';
-
-function App() {
-	const items = [
-		{
-			id: 'l2kj5',
-			label: 'Can I use React on a project?',
-			content:
-				'You can use React on any project you want. You can use React on any project you want.'
-		},
-		{
-			id: 'l2kj6',
-			label: 'Can I use JavaScript on a project?',
-			content:
-				'You can use React on any project you want. You can use React on any project you want.'
-		},
-		{
-			id: 'l2kj7',
-			label: 'Can I use CSS on a project?',
-			content:
-				'You can use React on any project you want. You can use React on any project you want.'
-		}
-	];
-
-	return <Accordion items={items} />;
-}
+function App() {}
 
 export default App;
