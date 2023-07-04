b0d994aff4a32149112a5ee5fcd70444467555b4
Jarod Kober <jarod.development@gmail.com>
2023-07-04 14:51:01 -0700

    Created Dropdown Page
    * Created a component for a Dropdown page
    * Shifted all logic from the App component into the new page

---
+++ src/pages/DropdownPage.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import Dropdown from '../components/Dropdown';
+
+function DropdownPage() {
+	const [selection, setSelection] = useState(null);
+
+	const handleSelect = (option) => {
+		setSelection(option);
+	};
+
+	const options = [
+		{ label: 'Red', value: 'red' },
+		{ label: 'Green', value: 'green' },
+		{ label: 'Blue', value: 'blue' }
+	];
+
+	return (
+		<Dropdown
+			onChange={handleSelect}
+			options={options}
+			value={selection}
+		/>
+	);
+}
+
+export default DropdownPage;
