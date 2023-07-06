61237fdfabf6a796fe5250b8b3ef85cefc9caf87
Jarod Kober <jarod.development@gmail.com>
2023-07-06 12:30:23 -0700

    Implemented Modal Component
    * Created modal using a React portal to place the content outside of the default component structure to guarantee correct positioning
    * The component accepts actionBar and children props to allow for custom content to be placed in each modal
    * Applied useEffect to apply overflow hidden to prevent scrolling when the modal is open
    * Created basic initial modal styles
    * Updated the parent ModalPage component with a button to trigger the modal
    * Passed modal content down as props from ModalPage
    * Added div to contain modal on index.html

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,9 +1,47 @@
+import { useState } from 'react';
+import Button from '../components/Button';
 import Modal from '../components/Modal';
 
 function ModalPage() {
+	const [showModal, setShowModal] = useState(false);
+
+	const handleClick = () => {
+		setShowModal(true);
+	};
+
+	const handleClose = () => {
+		setShowModal(false);
+	};
+
+	const actionBar = (
+		<div>
+			<Button
+				onClick={handleClose}
+				primary
+			>
+				I accept
+			</Button>
+		</div>
+	);
+
+	const modal = (
+		<Modal
+			onClose={handleClose}
+			actionBar={actionBar}
+		>
+			An important agreement for you to accept.
+		</Modal>
+	);
+
 	return (
 		<div>
-			<Modal />
+			<Button
+				onClick={handleClick}
+				primary
+			>
+				Open Modal
+			</Button>
+			{showModal && modal}
 		</div>
 	);
 }
