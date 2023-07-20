cc74e797ec2240b494adbf6096111b2b7d00e9d8
Jarod Kober <jarod.development@gmail.com>
2023-07-20 14:00:33 -0700

    Created Counter Page and Custom Hook
    * Created CounterPage component that displays a count with a button to increment it, logging the value on increment
    * Added routing for the Counter page
    * Added the Counter page to the Sidebar component
    * Created a custom hook with the counter logic, so it's reusable when a counter is needed in the future

---
+++ src/pages/CounterPage.js
@@ -0,0 +1,20 @@
+import useCounter from '../hooks/use-counter';
+import Button from '../components/Button';
+
+function CounterPage({ initialCount }) {
+	const { count, increment } = useCounter(initialCount);
+
+	return (
+		<div>
+			<h1>Count is {count}</h1>
+			<Button
+				onClick={increment}
+				primary
+			>
+				Increment
+			</Button>
+		</div>
+	);
+}
+
+export default CounterPage;
